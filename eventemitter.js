f6bf17c938ac4a44aa17efd1a10cc3273d9d6b7f
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2015-04-09 17:05:37 +0200

    do not need to use tryCatch

--- eventemitter.js
+++ eventemitter.js
@@ -31,9 +31,11 @@ EventEmitter.prototype.trigger = function(evt, arg) {
   if (!this.__listeners.hasOwnProperty(evt)) return;
   var listeners = this.__listeners[evt].slice();
   _.each(listeners, listener => {
-    var err = _.tryCatch(() => listener(arg));
-    if (err instanceof Error)
+    try {
+      listener(arg);
+    } catch(e) {
       console.error(err, err.stack);
+    }
   });
 };
 
