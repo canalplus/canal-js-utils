c76562463163ce93cf26a77a5b3a0bd2f0241978
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2015-08-17 10:23:49 +0200

    uri: use URIjs for joining and resolving

--- url.js
+++ url.js
@@ -1,34 +1,16 @@
 var _ = require("./misc");
+var URIjs = require("URIjs");
 
-var schemeRe = /:\/\//;
-var uniqSchemeRe = /:\//;
-var selfDirRe = /\/\.\//g;
-var prevDirRe = /\/\.\.\//;
-var rmPrevDirRe = /[^\/]*\/+\.\.\//;
-var slashesRe = /\/+/g;
+function _joinUrl(baseUri, part) {
+  if (part.length === 0)
+    return baseUri;
 
-function _joinUrl(base, part) {
-  if (part.length === 0) return base;
-  if (schemeRe.test(part)) {
-    return part;
+  var partUri = new URIjs(part);
+  if (partUri.is("relative")) {
+    return partUri.absoluteTo(baseUri);
   } else {
-    return base + "/" + part;
+    return partUri;
   }
-}
-
-function _normalizeUrl(str) {
-  // remove ./ parts
-  if (selfDirRe.test(str))
-    str = str.replace(selfDirRe, "/");
-
-  // remove foo/../ parts
-  while(prevDirRe.test(str))
-    str = str.replace(rmPrevDirRe, "/");
-
-  // join multiple / except the scheme one
-  return str
-    .replace(slashesRe, "/")
-    .replace(uniqSchemeRe, "://");
 }
 
 function resolveURL() {
@@ -36,17 +18,13 @@ function resolveURL() {
   var len = args.length;
   if (len === 0)
     return "";
-  else
-    return _normalizeUrl(_.reduce(args, _joinUrl, ""));
+  else {
+    return URIjs.decode(_.reduce(args, _joinUrl, new URIjs("")));
+  }
 }
 
 function parseBaseURL(url) {
-  var slash = url.lastIndexOf("/");
-  if (slash >= 0) {
-    return url.substring(0, slash + 1);
-  } else {
-    return url;
-  }
+  return URIjs.commonPath(url, url);
 }
 
 module.exports = { resolveURL, parseBaseURL };
